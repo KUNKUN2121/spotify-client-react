24513cc9e27d2bf344d16cbc3637a2ef27fc687d
KUNKUN2121 <42810210+KUNKUN2121@users.noreply.github.com>
2024-05-17 20:48:09 +0900

    歌詞完成

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,18 +6,28 @@ import TopInfo from './templates/TopInfo/TopInfo';
 import Lyrics from './templates/Lyrics/Lyrics';
 
 const Home = () => {
+    const convertTimeView = (milliseconds) => {
+        const totalSeconds = Math.floor(milliseconds / 1000);
+        const minutes = Math.floor(totalSeconds / 60);
+        const seconds = totalSeconds % 60;
+        const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+        const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+        return `${formattedMinutes}:${formattedSeconds}`;
+    };
+
     const [now, setNow] = useState();
     useEffect(() => {
         async function fetchData() {
           const response = await fetch('http://100.73.31.2/api/now?room_id=xRouwJ6jx51gv0WPNdWv1kpcaFO5La4d');
           const data = await response.json();
+          
           setNow(data);
         //   console.log(data['progress_ms']);
         }
     
         const interval = setInterval(() => {
           fetchData();
-        }, 1000);
+        }, 2000);
     
         return () => clearInterval(interval);
       }, []);
@@ -26,8 +36,8 @@ const Home = () => {
         <div className="progress">
             <div className="bar"></div>
             <div className="time">
-                <p className="progress">00:00</p>
-                <p className="duration">00:00</p>
+                <p className="progress">{convertTimeView(now ? now.progress_ms : "")}</p>
+                <p className="duration">{convertTimeView(now ? now.duration_ms : "")}</p>
             </div>
         </div>
         {now ? <TopInfo now={now}/> : ""} 
--- src/components/templates/Lyrics/Lyrics.jsx
+++ src/components/templates/Lyrics/Lyrics.jsx
@@ -1,135 +1,60 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState , CSSProperties} from 'react'
 import './Lyrics.css'
+import { Lrc, useRecoverAutoScrollImmediately  } from 'react-lrc';
+// import Lrc from 'react-lrc'; 
 const Lyrics = ({now}) => {
-    // console.log(now.lyrics.syncedLyrics);
-    // var lyricsData = parseLRC(now.lyrics.syncedLyrics);
-    const [lyricsData, setLyricsData] = useState([]);
-    // if(now.links.song-id);
-    const [count, setCount] = useState(-1);
-    const [lyrics, setLyrics] = useState();
-    const lyricsAreaRef = useRef(null);
     const prevTitleRef = useRef(now.title);
-
-
-
-    // console.log(lyricsData)
-    
-
-    // useEffect(() => {
-    //     const interval = setInterval(() => {
-    //         setCount(prevCount => prevCount + 1);
-    //         console.log(now['progress_ms'])
-    //     }, 1000);
-    //     return () => clearInterval(interval);
-    // }, []);
-
-
-    useEffect(() => {
-        if (lyricsAreaRef.current) {
-            const container = lyricsAreaRef.current;
-            const targetElement = document.getElementById(count);
-            if (container && targetElement) {
-                const containerRect = container.getBoundingClientRect();
-                const targetRect = targetElement.getBoundingClientRect();
-                const offset = targetRect.top - containerRect.top - (containerRect.height - targetRect.height) / 2;
-
-                container.scrollTo({
-                    top: container.scrollTop + offset,
-                    behavior: 'smooth'
-                });
-            }
-        }
-    }, [count]);
+    const scrollRef = useRef(null);
 
     // 曲が変わったときの処理
     useEffect(() => {
         if(prevTitleRef.current !== now.title){
             prevTitleRef.current = now.title; // タイトルを変更
-            setCount(0); // 歌詞の位置を0に
-            const newLyricsData = parseLRC(now.lyrics.syncedLyrics); // 歌詞を変更
-            setLyricsData(newLyricsData); 
         }
     }, [now.title]);
 
-    useEffect(() => {
-        console.log('aaa');
-        const progressSec = now["progress_ms"] / 1000;
-        if (lyricsData && lyricsData.length > 0) {
-            console.log('bbb')
-            const closestLine = lyricsData.reduce((prev, curr) => {
-                const prevTime = prev.timestamp;
-                const currTime = curr.timestamp;
-                const progressTime = now['progress_ms'] / 1000;
+      // スクロールのリセット処理
+  useRecoverAutoScrollImmediately({
+    currentRef: scrollRef,
+    isPlaying: true,
+    time: now.progress_ms
+  });
 
-                return Math.abs(currTime - progressTime) < Math.abs(prevTime - progressTime) ? curr : prev;
-            }, { timestamp: '0', text: '', count: -1 });
+      const lineRenderer = ({ index, active, line}) => {
+        return (
+            <p
+                key={index}
+                style={{
+                    color: active ? 'red' : 'black',
+                    transition: 'color 0.3s ease',
+                    fontSize: '32px',
+                    textAlign: 'center',
+                }}
+            >
+                {line.content}
+            </p>
+        );
+    };
 
-            setCount(closestLine.count);
-        }
-
-    }, [now["progress_ms"]]);
-
-
-    
-    
-
-
+    const lrcStyle = {
+        flex: 1,
+        minHeight: 0,
+        overflowY: 'auto'  // 縦方向のスクロールを有効にする
+      };
 
   return (
-    <div>Lyrics
-        <div className="lyrics" ref={lyricsAreaRef}>
-            {lyricsData != null ? (
-                lyricsData.map(line => (
-                    <p 
-                        key={line.count} 
-                        id={line.count}
-                        style={{ color: line.count === count ? 'red' : 'black' }}>
-                        {line.text}
-                    </p>
-                ))
-            ) : (
-                <p>aaaaa</p>
-            )}
-        </div>
+    <div className="lyrics" ref={scrollRef}>
+      <Lrc 
+        className="lrc"
+        lrc={now.lyrics.syncedLyrics}
+        currentMillisecond={now.progress_ms}
+        lineRenderer={lineRenderer}
+        style={lrcStyle}
+        scrollOffset={-120}
+      />
     </div>
+   
   )
 }
-
-
-const parseLRC = (data) => {
-    let lines;
-    try {
-        lines = data.split('\n');
-    } catch (error) {
-
-    }
-    const result = [];
-    let count = 0;
-    for (const line of lines) {
-      const match = line.match(/\[(\d+:\d+\.\d+)\](.+)/);
-      if (match) {
-        const timestamp = convertTimestampToSeconds(match[1]);
-        const text = match[2];
-        result.push({ timestamp, text, count });
-        count++;
-      }
-      else{
-        // console.log(line);
-        result.push({timestamp: null, text: line });
-      }
-    }
-
-    return result;
-}
-
-const convertTimestampToSeconds = (timestamp) => {
-    if (timestamp) {
-        const [minutes, seconds] = timestamp.split(':').map(parseFloat);
-        console.log('hello');
-        return minutes * 60 + seconds;
-    } else {
-        return 0; // または、アプリケーションに適した形で null ケースを処理してください
-    }
-};
 
 export default Lyrics
