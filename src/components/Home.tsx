d199dc87d89293e85c61137a49b654ec75fa0c9b
KUNKUN2121 <42810210+KUNKUN2121@users.noreply.github.com>
2024-05-17 22:27:29 +0900

    progressBarまで

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -16,25 +16,57 @@ const Home = () => {
     };
 
     const [now, setNow] = useState();
+    const [progress, setProgress] = useState(0);
+    const [progressBarPercent, setProgressBarPercent] = useState(0);
+    
+    // SpotifyAPIとの遅延時間を計算する関数
+    const getDelay = (data) =>{
+         const getSpotifyTime = new Date(data.get_spotify_timestamp);
+        const currentDate = new Date();
+        return currentDate.getTime() - getSpotifyTime.getTime();
+    }
+
+    // API取得
+    const fetchData = async () => {
+        console.log("fetching");
+        const response = await fetch('http://100.73.31.2/api/now?room_id=xRouwJ6jx51gv0WPNdWv1kpcaFO5La4d');
+        const data = await response.json();
+        data.progress_ms += getDelay(data);
+        setNow(data);
+    }
     useEffect(() => {
-        async function fetchData() {
-          const response = await fetch('http://100.73.31.2/api/now?room_id=xRouwJ6jx51gv0WPNdWv1kpcaFO5La4d');
-          const data = await response.json();
-          
-          setNow(data);
-        //   console.log(data['progress_ms']);
+        fetchData(); // 読み込み時に実行
+        const interval = setInterval(() => {
+          fetchData();
+        }, 5000);
+        return () => clearInterval(interval);
+      }, []);
+
+      useEffect(() => {
+        function abcd() {
+            if(now != null){
+                if(now.progress_ms <now.duration_ms) {
+                    setNow(prevNow => ({ ...prevNow, progress_ms: prevNow.progress_ms + 200 }));
+                    setProgressBarPercent(now.progress_ms / now.duration_ms * 100);
+                }else{
+                    fetchData();
+                }
+            }
         }
     
         const interval = setInterval(() => {
-          fetchData();
-        }, 2000);
+          abcd();
+        }, 200);
     
         return () => clearInterval(interval);
-      }, []);
+      }, [now]);
+
     return (
         <>
         <div className="progress">
-            <div className="bar"></div>
+            <div className="bar" style={{
+                backgroundSize: progressBarPercent + "%",
+            }}></div>
             <div className="time">
                 <p className="progress">{convertTimeView(now ? now.progress_ms : "")}</p>
                 <p className="duration">{convertTimeView(now ? now.duration_ms : "")}</p>
