e2329895512a494f731c9bdb2f09af5930d97d84
KUNKUN2121 <42810210+KUNKUN2121@users.noreply.github.com>
2024-05-27 18:26:20 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,12 @@ import Controller from './components/templates/Controller/Controller.jsx'
 import { useEffect, useState } from 'react';
 
 function App() {
+
+    var url = "http://100.73.31.2";
+    var roomId = "xRouwJ6jx51gv0WPNdWv1kpcaFO5La4d";
+
     const [now, setNow] = useState();
+    
     // SpotifyAPIとの遅延時間を計算する関数
     const getDelay = (data) =>{
         const getSpotifyTime = new Date(data.get_spotify_timestamp);
@@ -21,10 +26,9 @@ function App() {
     }
 
     // API取得
-    const roomId="xRouwJ6jx51gv0WPNdWv1kpcaFO5La4d";
     const fetchData = async () => {
         console.log("fetching");
-        const response = await fetch("http://100.73.31.2/api/now?room_id=" + roomId);
+        const response = await fetch(url + "/api/now?room_id=" + roomId);
         const data = await response.json();
         data.progress_ms += getDelay(data);
         setNow(data);
@@ -43,7 +47,6 @@ function App() {
                     if(now.is_playing == true){
                         if(now.progress_ms <now.duration_ms) {
                             setNow(prevNow => ({ ...prevNow, progress_ms: prevNow.progress_ms + 200 }));
-                            // setProgressBarPercent(now.progress_ms / now.duration_ms * 100);
                         }else{
                             fetchData();
                         }
@@ -64,11 +67,15 @@ function App() {
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<>
-                {now ? <ProgressBar now={now}/>: ""}
+                {now ? <ProgressBar now={now}/> : ""}
                 {now ? <TopInfo now={now}/> : ""} 
                 {now ? <Lyrics now={now}/> : ""}
                 {now ? <Controller now={now}/> : ""}  
                 {now ? <QueueList  now={now}/> : ""}
+            </>} />
+            <Route path="/add" element={<>
+                {now ? <ProgressBar now={now}/> : ""}
+                <AddMusic />
             </>} />
             
 
