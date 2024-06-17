1943f7e51ba37d19a2abbf2c82236ac37aa59e89
KUNKUN2121 <42810210+KUNKUN2121@users.noreply.github.com>
2024-06-17 20:15:12 +0900

    検索待機時間を変更

--- src/components/templates/AddMusic/AddMusic.jsx
+++ src/components/templates/AddMusic/AddMusic.jsx
@@ -24,7 +24,7 @@ const AddMusic = ({ roomId, url }) => {
         setSerachResultList: setSerachResultList,
         url: url,
       });
-    }, 800);
+    }, 500);
     setTimer(newTimer);
   };
 
