6c35af2522059894797fce5fc63da97838045f42
KUNKUN2121 <42810210+KUNKUN2121@users.noreply.github.com>
2024-06-20 00:17:01 +0900

    有効ボタン

--- src/components/templates/Controller/Controller.jsx
+++ src/components/templates/Controller/Controller.jsx
@@ -11,10 +11,21 @@ import LyricsIcon from '@mui/icons-material/Lyrics';
 import {Link} from 'react-router-dom';
 import "./Controller.css";
 
-const Controller = ({now, toggleQueueList, toggleSleep} ) => {
+const Controller = ({now, toggleQueueList , isLocked, release, request} ) => {
+    // <button type="button" onClick={() => (isLocked ? release() : request())}>
+    //     {isLocked ? "Release" : "Request"}
+    // </button>
   return (
     <div className="controller">
-        <button onClick={toggleSleep}><LightbulbIcon style={{fontSize: 32}}/></button>
+            <button type="button" onClick={() => (isLocked ? release() : request())}>
+        {isLocked ? 
+        // WakeOnLock 有効
+        <LightbulbIcon style={{fontSize: 32,}}/>
+        : 
+        <LightbulbIcon style={{fontSize: 32, opacity: 0.33 }}/>
+        }
+    </button>
+        {/* <button onClick={toggleSleep}><LightbulbIcon style={{fontSize: 32}}/></button> */}
         <button onClick={toggleQueueList}><QueueMusicIcon style={{fontSize: 32}} /></button>
         <button><Link to='/history'><HistoryIcon style={{fontSize: 32}}/></Link></button>
         
